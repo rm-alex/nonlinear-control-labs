f513b29737262e867207b746d77ddce1b5a10195
Alexey Rumyantsev <alexeyrumyantsev04@gmail.com>
2025-09-30 11:05:50 +0300

    com

--- lab1/src/scripts/mcpps.m
+++ lab1/src/scripts/mcpps.m
@@ -2,8 +2,8 @@
 syms x1 x2
 
 % change sys (1)
-dx1 = x2;
-dx2 = -x1+x2*(1-x1^2+0.1*x1^4);
+dx1 = (x1-x2)*(1-x1^2-x2^2);
+dx2 = (x1+x2)*(1-x1^2-x2^2);
 
 eqns = [dx1 == 0, dx2 == 0];
 S = solve(eqns, [x1, x2], 'Real', true);
@@ -25,8 +25,8 @@ end
 % x1=r*cos(theta);
 % x2=r*sin(theta);
 % 
-% dx1=-x1+2*x1^3+x2;
-% dx2=-x1-x2;
+% dx1=(x1-x2)*(1-x1^2-x2^2);
+% dx2=(x1+x2)*(1-x1^2-x2^2);
 %  
 % r_dot     = simplify((x1*dx1 + x2*dx2)/r);
 % theta_dot = simplify((x1*dx2 - x2*dx1)/r^2);
@@ -43,8 +43,8 @@ clc; clear; close all;
 syms x1 x2
 
 % change sys (2)
-dx1_sym = x2;
-dx2_sym = -x1+x2*(1-x1^2+0.1*x1^4);
+dx1_sym = (x1 - x2)*(1 - x1^2 - x2^2);
+dx2_sym = (x1 + x2)*(1 - x1^2 - x2^2);
 
 eqns = [dx1_sym == 0, dx2_sym == 0];
 S = solve(eqns, [x1, x2], 'Real', true);
@@ -54,8 +54,8 @@ x2_eq = double(S.x2);
 
 % change sys (3)
 [x1g, x2g] = meshgrid(-3:0.2:3, -3:0.2:3);
-dx1 = x2g;
-dx2 = -x1g+x2g.*(1-x1g.^2+0.1.*x1g.^4);
+dx1 = (x1g - x2g).*(1 - x1g.^2 - x2g.^2);
+dx2 = (x1g + x2g).*(1 - x1g.^2 - x2g.^2);
 
 L = sqrt(dx1.^2 + dx2.^2);
 dx1n = dx1 ./ (L + eps);
@@ -71,20 +71,25 @@ title('Фазовый портрет системы');
 % axis equal;
 
 % change sys (4)
-f = @(t,X) [X(2);
-            -X(1)+X(2)*(1-X(1)^2+0.1*X(1)^4)];
+f = @(t,X) [(X(1)-X(2))*(1 - X(1)^2 - X(2)^2);
+            (X(1)+X(2))*(1 - X(1)^2 - X(2)^2)];
 
-inits = [-0.1 0];
-tspan = [0 16];
+inits = [0 0.15; 0 -0.15; 2 0; -2 0; 0 2; 0 -2; -2 2; 2 -2; 1.5 2.5; -1.5 -2.5];
+tspan = [0 50];
 
 for i = 1:size(inits,1)
     [t,X] = ode15s(f, tspan, inits(i,:));
     h2 = plot(X(:,1), X(:,2), 'b', 'LineWidth', 0.5);
 end
 
-h3 = plot(x1_eq, x2_eq, 'ko', 'MarkerFaceColor','g', 'MarkerSize',6);
+% h3 = plot(x1_eq, x2_eq, 'ko', 'MarkerFaceColor','g', 'MarkerSize',6);
+h3 = plot(0, 0, 'ko', 'MarkerFaceColor','g', 'MarkerSize',6);
 
-legend([h1, h2, h3], ...
-       {'Поле направлений','Траектории','Точки равновесия'}, ...
+% sys 4 cycle
+theta = linspace(0,2*pi,200);
+h4 = plot(cos(theta), sin(theta), 'k', 'LineWidth',0.5);
+
+legend([h1, h2, h3, h4], ...
+       {'Поле направлений','Траектории','Точки равновесия', 'Предельный цикл'}, ...
        'Location','best');
 axis([-3 3 -3 3]);
